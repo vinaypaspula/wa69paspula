f51ead0be4c9dd024c2414144d2b9b0b7d57f13a
Vinay Kumar Paspula <vinaykmr149@gmail.com>
2021-03-24 17:53:11 -0500

    New endpoint

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var indexRouter = require('./routes/mydata');
+var mydataRouter = require('./routes/mydata');
 var usersRouter = require('./routes/users');
 var stuffRouter = require('./routes/stuff');
 
