4d6c8697e7d67d364ac9a43f50dacac55b50a698
Vinay Kumar Paspula <vinaykmr149@gmail.com>
2021-03-24 19:39:45 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,13 +1,13 @@
 var express=require('express')
-var router=express.Router();
+var router=express.Router()
 
 router.get('/',(req,res)=>{
     let randomNumber=Math.floor(Math.random()*100)+1
     let data=req.query.x
     console.log(data)
     res.render('computation',{
-        randSolution:`Cube root applied to ${randomNumber} is ${Math.cbrt(randomNumber)}`,
-        querySolution:`Cube root applied to ${data} is ${Math.cbrt(data)}`
+        randSolution:`Cube root of ${randomNumber} is ${Math.cbrt(randomNumber)}`,
+        querySolution:`Cube root of ${data} is ${Math.cbrt(data)}`
     })
 })
 
